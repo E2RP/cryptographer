5f7151ad74b139cffe6f11e7e550dd7b7fa3af91
alexander-p30 <alexanderp30e@gmail.com>
2021-06-05 20:37:44 -0300

    refactor key logic module list appending

--- lib/cryptographer/aes/key.ex
+++ lib/cryptographer/aes/key.ex
@@ -3,7 +3,7 @@ defmodule Cryptographer.Aes.Key do
 
   def generate(), do: 8 |> :crypto.strong_rand_bytes() |> Base.encode16()
 
-  def one_bit_rotation(word) do
+  def one_byte_left_rotate_word(word) do
     word
     |> Enum.with_index(0)
     |> Enum.reduce([], fn {char, i}, acc -> List.insert_at(acc, i - 1, char) end)
@@ -12,11 +12,11 @@ defmodule Cryptographer.Aes.Key do
 
   defp sub_byte(byte), do: Table.get_at(byte)
 
-  def sub_word(word), do: sub_word(word, [])
+  def sub_word(word), do: word |> sub_word([]) |> Enum.reverse()
   defp sub_word(<<>>, acc), do: acc
-  defp sub_word(<<byte::8, rest::binary>>, acc), do: sub_word(rest, acc ++ [sub_byte(byte)])
+  defp sub_word(<<byte::8, rest::binary>>, acc), do: sub_word(rest, [sub_byte(byte) | acc])
 
-  def round_constant(j), do: rc(j, 0x01)
-  defp rc(1, acc), do: acc * 0x01
-  defp rc(j, acc), do: rc(j - 1, 0x02 * acc)
+  def round_constant(j), do: round_constant(j, 0x01)
+  defp round_constant(1, acc), do: acc * 0x01
+  defp round_constant(j, acc), do: round_constant(j - 1, 0x02 * acc)
 end
