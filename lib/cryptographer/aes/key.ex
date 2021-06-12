35f8fc5a14131495030f22c94cdfd95bf0e6d568
alexander-p30 <alexanderp30e@gmail.com>
2021-06-12 16:28:09 -0300

    updates aes key generation to 16 bytes

--- lib/cryptographer/aes/key.ex
+++ lib/cryptographer/aes/key.ex
@@ -2,5 +2,5 @@ defmodule Cryptographer.Aes.Key do
   @type t :: <<_::128>>
 
   @spec generate() :: t()
-  def generate(), do: 12 |> :crypto.strong_rand_bytes() |> Base.encode64()
+  def generate(), do: 16 |> :crypto.strong_rand_bytes()
 end
