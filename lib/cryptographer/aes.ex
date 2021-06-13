3d3c3db3dd16d1be9d88b19b54fc82333c1cadcc
alexander-p30 <alexanderp30e@gmail.com>
2021-06-13 14:56:03 -0300

    introduces several typings for AES 128 bit words

--- lib/cryptographer/aes.ex
+++ lib/cryptographer/aes.ex
@@ -1,3 +1,9 @@
 defmodule Cryptographer.Aes do
-  @type word :: <<_::32>>
+  @type word_binary :: <<_::32>>
+  @type word_byte_list :: [byte(), ...]
+  @type word :: word_binary() | word_byte_list()
+
+  @type words_binary :: <<_::_*32>>
+  @type words_byte_list :: [word_byte_list(), ...]
+  @type words :: words_binary() | words_byte_list()
 end
