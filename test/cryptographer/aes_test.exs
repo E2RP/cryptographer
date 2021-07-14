e4dba9d8d40f0e2b7f5f2110aebd5e6c79a3352f
alexander-p30 <alexanderp30e@gmail.com>
2021-07-14 09:14:12 -0300

    introduce AES encryption test

--- test/cryptographer/aes_test.exs
+++ test/cryptographer/aes_test.exs
@@ -5,9 +5,10 @@ defmodule Cryptographer.AesTest do
   alias Cryptographer.Aes.Key.Scheduler
 
   describe "encrypt/2" do
-    test "anything idk" do
+    test "returns a correctly AES-128 encrypted message" do
       key = <<0x54686174::32, 0x73206D79::32, 0x204B756E::32, 0x67204675::32>>
       message = <<0x00000101::32, 0x03030707::32, 0x0F0F1F1F::32, 0x3F3F7F7F::32>>
+      result = <<0x00000101::32, 0x03030707::32, 0x0F0F1F1F::32, 0x3F3F7F7F::32>>
 
       assert Aes.encrypt(message, key) == result
     end
